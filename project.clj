729c127624c0aa78fb55c188095f71dd2985d03f
Robin Schreiber <robin.schreiber@me.com>
2015-07-26 10:26:14 +0200

    clean up dependencies

--- project.clj
+++ project.clj
@@ -4,29 +4,25 @@
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "1.0.6"]
-            [com.keminglabs/cljx "0.3.2" :exclusions [org.clojure/clojure]]
-            ]
+            [com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure]]]
   :source-paths ["src/clj" "src/cljs" "src/cljx" "test"]
-  :cljsbuild {:builds [{:source-paths ["src/cljs" "src/gen/clj" "src/gen/cljs" "test"]
-                        :output-path "cljs-build-shell"}]
-              :test-commands {"morph-tree-test" ["phantomjs" "phantom/unit-test.js"]}}
-;   :cljx {:builds [{:source-paths ["src/cljx"]
-;                   :output-path "src/gen/clj"
-;                   :rules :clj}
-;                   {:source-paths ["src/cljx"]
-;                   :output-path "src/gen/cljs"
-;                   :rules :cljs}]}
+  :cljsbuild {:builds {:main {:source-paths ["src/clj" "src/cljs" "src/gen/clj" "src/gen/cljs" "test"]
+                        :output-path "cloxp-cljs-build"}}
+              :test-commands {"morph-tree-test" ["slimerjs" "cloxp-cljs-build/cloxp-cljs.js"]}}
+  ; :cljx {:builds [{:source-paths ["src/cljx"]
+  ;                 :output-path "src/gen/clj"
+  ;                 :rules :clj}
+  ;                 {:source-paths ["src/cljx"]
+  ;                 :output-path "src/gen/cljs"
+  ;                 :rules :cljs}]}
   :dependencies
-  [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-3126"]
-   [org.rksm/cloxp-com "0.1.5"]
-   [http-kit "2.1.19"]
-   [compojure/compojure "1.3.2"]
-   [ring "1.3.1"]
-   [fogus/ring-edn "0.2.0"]
+  [[org.clojure/clojure "1.7.0"]
+   [org.clojure/clojurescript "0.0-3308"]
+   [org.rksm/cloxp-com "0.1.9-SNAPSHOT"]
    [om "0.8.0-rc1"]
-   [org.clojure/core.async "0.1.303.0-886421-alpha"]
+   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+   [fresnel "0.3.0-SNAPSHOT"]
    [cljs-tooling "0.1.7"]
    [bostonou/cljs-pprint "0.0.4-SNAPSHOT"]
-   [prismatic/om-tools "0.3.11"]
+   [fipp "0.6.2"]
    [org.clojure/core.match "0.3.0-alpha4"]])
