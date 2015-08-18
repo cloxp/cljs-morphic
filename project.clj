a10290c0387217f5db63c6cfff923222ca7f4def
Robin Schreiber <robin.schreiber@me.com>
2015-08-18 12:48:33 +0200

    updated deps

--- project.clj
+++ project.clj
@@ -4,25 +4,35 @@
   :license {:name "Eclipse Public License",
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-cljsbuild "1.0.6"]
-            [com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure]]]
+            [com.keminglabs/cljx "0.6.0" :exclusions [org.clojure/clojure]]
+			[com.cemerick/clojurescript.test "0.3.3"]]
+  :prep-tasks [["cljx" "once"] "javac" "compile"]
   :source-paths ["src/clj" "src/cljs" "src/cljx" "test"]
   :cljsbuild {:builds {:main {:source-paths ["src/clj" "src/cljs" "src/gen/clj" "src/gen/cljs" "test"]
-                        :output-path "cloxp-cljs-build"}}
-              :test-commands {"morph-tree-test" ["slimerjs" "cloxp-cljs-build/cloxp-cljs.js"]}}
-  ; :cljx {:builds [{:source-paths ["src/cljx"]
-  ;                 :output-path "src/gen/clj"
-  ;                 :rules :clj}
-  ;                 {:source-paths ["src/cljx"]
-  ;                 :output-path "src/gen/cljs"
-  ;                 :rules :cljs}]}
+                  						:compiler {:optimizations :whitespace
+                               					 :output-dir "target/out"
+                            						 :output-to "target/cloxp-cljs.js"
+                                         :source-map "target/cloxp-cljs.js.map"}}}
+         			  :test-commands {"unit-tests" ["phantomjs" :runner "target/cloxp-cljs.js"]}}
+  :cljx {:builds [{:source-paths ["src/cljx"]
+                  :output-path "src/gen/clj"
+                  :rules :clj}
+                  {:source-paths ["src/cljx"]
+                  :output-path "src/gen/cljs"
+                  :rules :cljs}]}
   :dependencies
   [[org.clojure/clojure "1.7.0"]
-   [org.clojure/clojurescript "0.0-3308"]
+   [org.clojure/clojurescript "1.7.28"]
    [org.rksm/cloxp-com "0.1.9-SNAPSHOT"]
-   [om "0.8.0-rc1"]
+   [org.omcljs/om "0.9.0" :exclusions [cljsjs/react]]
+   [cljsjs/react-with-addons "0.13.3-0"]
    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-   [fresnel "0.3.0-SNAPSHOT"]
+   [fresnel "0.3.0-SNAPSHOT" :exclusions [org.clojure/clojure]]
    [cljs-tooling "0.1.7"]
    [bostonou/cljs-pprint "0.0.4-SNAPSHOT"]
    [fipp "0.6.2"]
-   [org.clojure/core.match "0.3.0-alpha4"]])
+   [org.clojure/core.match "0.3.0-alpha4"]
+   [pjstadig/humane-test-output "0.7.0"]]
+  :injections 
+  [(require 'pjstadig.humane-test-output)
+   (pjstadig.humane-test-output/activate!)])
