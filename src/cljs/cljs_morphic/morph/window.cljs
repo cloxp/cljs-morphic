6cc0de8a85555cc0f5a5475419391c37fef7fcbb
Robin Schreiber <robin.schreiber@me.com>
2015-08-27 23:56:37 +0200

    add controls to window frame

--- src/cljs/cljs_morphic/morph/window.cljs
+++ src/cljs/cljs_morphic/morph/window.cljs
@@ -9,21 +9,24 @@
 (morph-fn layout-morph [self props submorphs]
      ((props :layout) self props submorphs))
 
-(def control-height 12)
+(def control-height 14)
 
 (defn close-box [target-id]
   (ellipse
-   {:position {:x 10 :y 10}
+   {:id (str "close-on-" target-id)
+    :position {:x 10 :y 10}
     :fill "#ff6052"
-    :mouse-enter '(fn [world] 
-                    (let [x-box (-> self :>> :submorphs first)]
-                      (om/update! x-box [:morph :Visible] true)))
-    :mouse-leave '(fn [world] 
-                    (let [x-box (-> self :>> :submorphs first)]
-                      (om/update! x-box [:morph :Visible] false)))
-    :mouse-click '(fn [world]
-                    (let [target (-> self :>> :morph :target-id)]
-                      (morphic/remove-morph! ($morph "World") target)))
+    :mouse-enter '(fn [world self-ref] 
+                    (redefine world self-ref 
+                                (fn [self props submorphs]
+                                  (self props (set-prop submorphs "close-label" :visible true)))))
+    :mouse-leave '(fn [world self-ref] 
+                    (redefine world self-ref 
+                                (fn [self props submorphs]
+                                  (self props (set-prop submorphs "close-label" :visible false)))))
+    :mouse-click '(fn [world self-ref]
+                    (let [target (fetch world [self-ref properties :target-id])]
+                      (without world target)))
     :extent {:x control-height :y control-height}
     :target-id target-id} 
    (text {:position {:x -2 :y -6}
@@ -31,52 +34,63 @@
           :text-string "×"
           :font-size 10
           :allow-input false
-          :visible true})))
+          :visible false})))
 
 (defn min-box [target-id]
   (ellipse 
-   {:position {:x 30 :y 10}
+   {:id (str "min-box-on-" target-id)
+    :position {:x 30 :y 10}
     :extent {:x control-height :y control-height}
     :fill "#ffbe06"
-    :mouse-enter '(fn [world id] 
-                     (let [x-box (-> self :>> :submorphs first)]
-                       (om/update! x-box [:morph :Visible] true)))
-    :mose-leave '(fn [world id] 
-                     (let [x-box (-> self :>> :submorphs first)]
-                       (om/update! x-box [:morph :Visible] false)))
-    :mouse-click '(fn [world id]
-                    (let [target (fetch world [self :properties :target-id])
+    :mouse-enter '(fn [world self-ref] 
+                    (redefine world self-ref 
+                                (fn [self props submorphs]
+                                  (self props (set-prop submorphs "min-label" :visible true)))))
+    :mouse-leave '(fn [world self-ref] 
+                    (redefine world self-ref 
+                                (fn [self props submorphs]
+                                  (self props (set-prop submorphs "min-label" :visible false)))))
+    :mouse-click '(fn [world self-ref]
+                    (let [target (fetch world [self-ref :properties :target-id])
                           width (fetch world [($morph target) :properties :extent :x])]
-                  (morphic/set-prop world target {:x width :y 30})))
+                  (set-prop world target {:x width :y 30})))
     :target-id target-id}
    (text {:position {:x -2 :y -6}
           :id "min-label"
           :text-string "−"
           :font-size 10
           :allow-input false
-          :visible true})))
+          :visible false})))
+
+(morph-fn window-controls [window props submorphs]
+          (window props submorphs
+                  (close-box (:id props))
+                  (min-box (:id props))))
 
 (morph-fn window-resizer [window props submorphs]
           (window props submorphs
                   (-> (image {:url "http://localhost:8084/resize.png" 
                               :id (str "resizer-on-" (props :id))
+                              :window-id (:id props)
+                              :target-id (:target-id props)
                               :opacity 0.3
                               :draggable? true
                               :position (add-points (props :extent) {:x -25 :y -25})
                               :extent {:x 25 :y 25} 
                               :css {"cursor" "nwse-resize"}
-                              :on-drag (fn [world id]
-                                         (let [[_ {pos :position} _] (fetch world ($morph id))
+                              :on-drag '(fn [world id]
+                                         (let [[_ {pos :position win :window-id target :target-id} _] (fetch world ($morph id))
                                                new-extent (add-points pos {:x 25 :y 25})]
                                            (-> world
-                                             (set-prop (:id props) :extent new-extent)
-                                             (set-prop  (:target-id props) :extent (add-points {:x 0 :y -30} new-extent)))))}))))
+                                             (set-prop win :extent new-extent)
+                                             (set-prop target :extent (add-points {:x 0 :y -30} new-extent)))))}))))
 
 (morph-fn window [self props submorphs]
           (let [window-ext (add-points {:x 0 :y 30} (props :extent))
                 window-pos (add-points {:x 0 :y -30} (props :position))
                 window-id  (str "window-on-" (props :id))]
             (-> (rectangle {:id window-id
+                            :inspectable? true
                           :position window-pos
                           :extent window-ext
                           :border-color "grey"
@@ -87,4 +101,5 @@
                           :draggable? true
                           :target-id (:id props)}
                          (self (assoc props :position {:x 0 :y 30} :draggable? false) submorphs))
+              window-controls
               window-resizer)))
